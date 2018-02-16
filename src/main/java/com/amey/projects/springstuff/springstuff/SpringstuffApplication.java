c5c717fd7760821cb17193ee4187eaa1e886e46a
Amey Tripathi <amey.tripathi@absa.co.za>
2018-02-16 11:14:55 +0200

    testing simple apache commons crypto lib

---
+++ src/main/java/com/amey/projects/springstuff/springstuff/SpringstuffApplication.java
@@ -0,0 +1,12 @@
+package com.amey.projects.springstuff.springstuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringstuffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringstuffApplication.class, args);
+	}
+}
